dba5ad521565bdb79bb1104a5991c41272bded8d
LuccaRex <luccafuso2017@gmail.com>
2023-10-19 16:55:30 -0400

    create descripto

--- server.js
+++ server.js
@@ -7,5 +7,12 @@ app.listen(4000, () => { console.log("tá rodando...")});
 
 app.post("/cripto", async function cripto(req, res){
     const { password } = req.body;
-    return res.send(password);
+    const newpassword = await bcrypt.hash(password, 10);
+    return res.send(newpassword);
+});
+
+app.post("/descripto", async function descripto(req, res){
+    const { password, newpassword } = req.body;
+    const response = await bcrypt.compare(password, newpassword);
+    return res.send(response);
 });
